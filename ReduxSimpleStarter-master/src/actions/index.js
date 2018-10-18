ed97b80887819e0ac1bd375bd684c09a845056ad
johnnyboldt <johnny.boldt@gmail.com>
2018-10-18 09:49:17 -0600

    getting weather data from third party

---
+++ ReduxSimpleStarter-master/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'c21bce1c1cdc8c2e84771cdb3b13de0f';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+    const url = `${ROOT_URL}&q=${city},can`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
