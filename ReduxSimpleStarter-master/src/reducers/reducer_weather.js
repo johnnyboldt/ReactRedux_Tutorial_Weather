337829a6ccd44d4d16a2ba77b44d5fa7cb7a4855
Johnny Boldt <johnny.boldt@absorblms.com>
2018-11-07 11:20:38 -0700

    finished weather app

--- ReduxSimpleStarter-master/src/reducers/reducer_weather.js
+++ ReduxSimpleStarter-master/src/reducers/reducer_weather.js
@@ -1,3 +1,17 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index'
+
+export default function(state = [], action) {
+    if (action.error) {
+        alert('City not found! Check city spelling.');
+        return state;
+      }  
+
+    switch(action.type)
+    {
+        case FETCH_WEATHER:
+            //return state.concat([action.payload.data ]); //We never manipulate old state with something like state.push, we create new state
+            return [action.payload.data, ...state]; // [city, city, city]
+    }
     return state;
 }
+
